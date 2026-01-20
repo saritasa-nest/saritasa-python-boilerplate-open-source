65c917716d21a50cb865d32cd2f1e66b19fa8b91
Stanislav Khlud <stanislav.khlud@saritasa.com>
2026-01-20 13:18:49 +0700

    Init boilerplate

---
+++ template/tasks.py
@@ -0,0 +1,27 @@
+import invoke
+import saritasa_invocations
+
+import invocations
+
+ns = invoke.Collection(
+    invocations.project,
+    saritasa_invocations.pytest,
+    saritasa_invocations.poetry,
+    saritasa_invocations.git,
+    saritasa_invocations.pre_commit,
+    saritasa_invocations.mypy,
+    saritasa_invocations.python,
+)
+
+# Configurations for run command
+ns.configure(
+    {
+        "run": {
+            "pty": True,
+            "echo": True,
+        },
+        "saritasa_invocations": saritasa_invocations.Config(
+            project_name="{{ package_name }}",
+        ),
+    },
+)
